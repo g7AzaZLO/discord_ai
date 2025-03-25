33ba9483f439cf344a7714f5c179c0eb2e641876
AzaZLO <MaloyMeee@yandex.ru>
2025-03-25 17:01:15 +0300

    60 sec

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
-openai_api_key = "YOUR_OPENAI_API_KEY"
+openai_api_key = "sk-QMOTsnkK5AloFHdD9sobGQ"
 openai_model = "gpt-3.5-turbo"
 
 
 # You can use the proxy if your local IP is from the blocked country in that format: http://username:password@ip:port. Leave "NONE" if you're not going to use it.
-proxy_for_openai_api = "None"
+proxy_for_openai_api = "http://proxy1:proxy1@87.120.84.55:55432"
 
 # min time to send the next message (in secs)
 interval_time_from = 60
